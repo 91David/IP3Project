099216c0b120aa4eed73bb48dcd4dd0c27d1eaaf
ScoBrun <scott.brunton366@gmail.com>
2017-02-17 16:07:30 +0000

    Written functionality for getting a filename and generating checksum. TODO: Convert to hex and return.

--- src/Document.java
+++ src/Document.java
@@ -1,4 +1,46 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 class Document {
 
+    /**
+     * Generates MD5 checksum from file.
+     *
+     * @param filename
+     * @return
+     */
+    public static String generateChecksum(String filename) {
+
+        try {
+            // Instantiating file and Hashing Algorithm.
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            FileInputStream file = new FileInputStream(filename);
+
+            // Generation of checksum.
+            byte[] dataBytes = new byte[1024];
+            int nread;
+
+            while ((nread = file.read(dataBytes)) != -1) {
+                md.update(dataBytes, 0, nread);
+            }
+
+            byte[] mdbytes = md.digest();
+
+            // TODO - convert byte to hex.
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 
 }
