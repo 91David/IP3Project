8782f51c6a4d78bbf24be1e8ba63ddce52d3671f
Wwbrett <wbrett200@caledonian.ac.uk>
2017-03-21 14:19:04 +0000

    Animation changes
    The input boxes are easier to read as well as looking nicer. Also messed with some shadows with the navigation and content so the user can clearly see what is where.

--- webApp/src/main/resources/webapp/Javascript/sideNav.js
+++ webApp/src/main/resources/webapp/Javascript/sideNav.js
@@ -1,20 +1,13 @@
 $(function () {
     var wrapper = $(".wrapper"),
         toggle = $(".toggle"),
+        table = $("#mainTable"),
         nav = $(".side-nav");
     toggle.on("click", function () {
         wrapper.toggleClass("nav-open");
+        table.toggleClass("noShadow");
         // Change the font-awesome icons on click.
         toggle.toggleClass("fa-bars");
         toggle.toggleClass("fa-times");
-    });
-
-    $(window).on("click", function (e) {
-        if (wrapper.hasClass("nav-open") &&
-            !$(e.target).hasClass("toggle")) {
-            //wrapper.removeClass("nav-open");
-            //toggle.toggleClass("fa-bars");
-            //toggle.toggleClass("fa-times");
-        }
     });
 });
